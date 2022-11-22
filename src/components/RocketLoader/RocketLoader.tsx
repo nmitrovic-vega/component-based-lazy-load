666c223d68b2a0a07ed8670c563de90020a0f6fd
Nikola Mitrovic <n.mitrovic@vegait.rs>
2022-11-22 07:20:39 +0100

    After LLLL.

--- src/components/RocketLoader/RocketLoader.tsx
+++ src/components/RocketLoader/RocketLoader.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
-import { ReactComponent as RocketLoading } from '../../assets/images/launch.svg';
-import './RocketLoader.css';
+import React from "react";
+import * as moment from "moment";
+import * as momentTZ from "moment-timezone";
+import * as lodash from "lodash";
+import * as rxjs from "rxjs";
+import * as formik from "formik";
+import * as reactHookForm from "react-hook-form";
+import * as reactQuery from "react-query";
+import * as underscore from "underscore";
+import * as ramda from "ramda";
+import axios from "axios";
+import { ReactComponent as RocketLoading } from "../../assets/images/launch.svg";
+import "./RocketLoader.css";
+import { useEffect } from "react";
 
 const RocketLoader = () => {
-  return (
-    <div className="rocket-loader__wrapper">
-      <div className="pulse rocket-loader">
-        <RocketLoading width="100" height="100" />
-      </div>
-    </div>
-  );
+	// component with performance bottleneck
+	useEffect(() => {
+		for (let i = 0; i < 100_000; i++) {
+			console.log("hola");
+		}
+	}, []);
+
+	return (
+		<div className="rocket-loader__wrapper">
+			<div className="pulse rocket-loader">
+				<RocketLoading width="100" height="100" />
+			</div>
+		</div>
+	);
 };
 
 export default RocketLoader;
